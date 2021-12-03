bcff6b219d24e186d1c1ebce57797c68151a882d
SBlasL <87399793+SBlasL@users.noreply.github.com>
2021-12-02 23:50:14 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+  <center>Paquetes de Clases</center>
+  );
+}
+
+export default App;
---
+++ src/AppEcommerce.jsx
@@ -0,0 +1,15 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+
+    return (
+        <center>
+            <NavBar />
+            Paquetes de Clases
+            <ItemListContainer greeting='¡Bienvenidx! A continuación verá el listado de clases disponibles.' />
+        </center>
+    )
+
+
+}
